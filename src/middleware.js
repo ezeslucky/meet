acce8252316eca17c11717e674d8ae317acefd17
Lucky <lucky123@gmail.com>
2024-11-12 10:32:51 +0530

    front

--- src/middleware.js
+++ src/middleware.js
@@ -6,12 +6,12 @@ import { NextResponse } from "next/server";
 export async function middleware(req){
     const token = await getToken({req,secret:process.env.NEXTAUTH_SECRET});
 
-    //if user try to go /user-auth after login
+    
     if(req.nextUrl.pathname === '/user-auth' && token){
         return NextResponse.redirect(new URL('/',req.url));
     }
 
-    //if user try to acess home page without login
+   
     if(!token && req.nextUrl.pathname !== '/user-auth'){
         return NextResponse.redirect(new URL('/user-auth',req.url));
     }
